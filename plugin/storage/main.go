b4808f6a983850f82c9a8f6f2b9b6be5ad1efc81
Omar Polo <op@omarpolo.com>
2025-10-07 17:15:26 +0000

    storage: pledge "stdio rpath inet dns"; with unveil too
    The s3 storage ideally only needs to do stdio and open sockets.  Since
    there's TLS in the mix we also need the golang stdlib to access the
    right cert.pem.
    
    Here the thing gets a little bit more complicated.  The stdlib tries
    to open cert.pem at "well-known" locations, including paths that don't
    make sense on OpenBSD (e.g. /usr/local/etc/ssl/cert.pem).
    
    To prevent that, set SSL_CERT_FILE to /etc/ssl/cert.pem, but only if
    it's not already set, and unveil that path.

--- plugin/storage/main.go
+++ plugin/storage/main.go
@@ -1,12 +1,34 @@
 package main
 
 import (
+	"log"
 	"os"
+	"runtime"
 
 	sdk "github.com/PlakarKorp/go-kloset-sdk"
+	s3 "github.com/PlakarKorp/integration-s3"
 	"github.com/PlakarKorp/integration-s3/storage"
 )
 
 func main() {
+	// golang stdlib tries to open cert files at "well known"
+	// locations.  On OpenBSD, we only really have
+	// /etc/ssl/cert.pem, so that's a safe guess, but attempt to
+	// respect SSL_CERT_FILE if set.
+	if runtime.GOOS == "openbsd" {
+		cert, ok := os.LookupEnv("SSL_CERT_FILE")
+		if !ok {
+			cert = "/etc/ssl/cert.pem"
+			os.Setenv("SSL_CERT_FILE", cert)
+		}
+
+		if err := s3.Unveil(cert, "r"); err != nil {
+			log.Fatalln("unveil /etc/ssl/cert.pem:", err)
+		}
+		if err := s3.Pledge("stdio rpath inet dns"); err != nil {
+			log.Fatalln("pledge:", err)
+		}
+	}
+
 	sdk.EntrypointStorage(os.Args, storage.NewStore)
 }
